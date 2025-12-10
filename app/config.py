bf4d3becde259276209382bb977f7e13352fad98
Karan gupta <gupta.karan1.gh@gmail.com>
2025-12-10 23:20:58 +0530

    list of ids of youtube channels

---
+++ app/config.py
@@ -0,0 +1,11 @@
+"""
+Static configuration for scraping.
+
+Only contains channel IDs we actively scrape; keep secrets in environment vars.
+"""
+
+YOUTUBE_CHANNELS = [
+    # "UCn8ujwUInbJkBhffxqAPBVQ", 
+    "UCawZsQWqfGSbCI5yjkdVkTA", 
+]
+
