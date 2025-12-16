063d248a32793879208b146d51bcf5f3d7daca9b
Karan gupta <gupta.karan1.gh@gmail.com>
2025-12-16 17:45:18 +0530

    allowed all origins

--- app/api/server.py
+++ app/api/server.py
@@ -35,10 +35,11 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="AI Notify API", version="1.0.0", lifespan=lifespan)
 
-origins = [os.getenv("FRONTEND_URL", "http://localhost:3000")]
+# origins = ["*"]
+# origins = [os.getenv("FRONTEND_URL", "http://localhost:3000")]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
